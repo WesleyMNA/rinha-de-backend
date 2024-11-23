4284d4b518d0e30fecb837a46386bd4344acf647
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-11-23 12:22:58 -0300

    #FEAT: validação de valores null e repetidos

---
+++ java/src/main/java/com/rinha/java/GlobalHandler.java
@@ -0,0 +1,27 @@
+package com.rinha.java;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseEntity<Void>> handleGlobalException(Exception ex, WebRequest request) {
+        System.out.println(ex.getClass());
+
+        if (ex instanceof DataIntegrityViolationException)
+            return ResponseEntity
+                    .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .build();
+
+        return ResponseEntity
+                .badRequest()
+                .build();
+    }
+}
--- java/src/main/java/com/rinha/java/pessoas/PessoaRequest.java
+++ java/src/main/java/com/rinha/java/pessoas/PessoaRequest.java
@@ -1,22 +1,14 @@
 package com.rinha.java.pessoas;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record PessoaRequest(
-        @NotNull
-        @NotEmpty
         @Size(max = 32)
         String apelido,
-        @NotNull
-        @NotEmpty
         @Size(max = 100)
         String nome,
-        @NotNull
-        @NotEmpty
         @Size(max = 10)
         String nascimento,
         List<String> stack
