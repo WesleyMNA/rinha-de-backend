59980602f36aa70ec2c93f0c7ea05c52771dae3e
Wesley Almeida <wesleymna98@gmail.com>
2024-11-25 14:47:24 -0300

    #FEAT: create pessoa

--- piton/src/controller.py
+++ piton/src/controller.py
@@ -1,14 +1,17 @@
+import uuid
 from typing import Annotated, List
-from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.params import Query
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.functions import func
 from starlette import status
+from starlette.responses import Response
 
 from src.db import get_db, Pessoa
-from src.dtos import PessoaResponse
+from src.dtos import PessoaResponse, PessoaRequest
 
 DbSession = Annotated[AsyncSession, Depends(get_db)]
 pessoa_router = APIRouter(prefix='/pessoas')
@@ -35,7 +38,7 @@ async def find_by_termo(
     response_model=PessoaResponse
 )
 async def find_by_id(
-        pessoa_id: UUID,
+        pessoa_id: uuid.UUID,
         db: DbSession,
 ):
     query = select(Pessoa).where(Pessoa.id == pessoa_id)
@@ -47,12 +50,43 @@ async def find_by_id(
     return pessoa
 
 
-@pessoa_router.post('')
+async def invalid(request: PessoaRequest):
+    return (
+            request.nome.isdigit() or
+            request.apelido.isdigit() or
+            (
+                    request.stack is not None
+                    and any(s.isdigit() for s in request.stack)
+            )
+    )
+
+
+@pessoa_router.post(
+    '',
+    status_code=status.HTTP_201_CREATED,
+)
 async def create(
-        request,
+        request: PessoaRequest,
         db: DbSession,
+        response: Response
 ):
-    pass
+    if await invalid(request):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+    pessoa_id = uuid.uuid4()
+    query = insert(Pessoa).values({
+        'id': pessoa_id,
+        **request.model_dump(),
+    })
+
+    try:
+        await db.execute(query)
+        await db.commit()
+    except:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    response.headers['Location'] = f'/pessoas/{pessoa_id}'
+    return pessoa_id
 
 
 main_router = APIRouter()
@@ -60,5 +94,7 @@ main_router.include_router(pessoa_router)
 
 
 @main_router.get('/contagem-pessoas')
-async def count():
-    pass
+async def count(db: DbSession):
+    query = select(func.count(Pessoa.id))
+    result = await db.execute(query)
+    return result.scalar()
--- piton/src/dtos.py
+++ piton/src/dtos.py
@@ -8,10 +8,9 @@ from pydantic import BaseModel, Field
 class PessoaRequest(BaseModel):
     apelido: str = Field(..., max_length=32)
     nome: str = Field(..., max_length=100)
-    nascimento: str = Field(..., max_length=10)
+    nascimento: date = Field(...)
     stack: List[str] | None = None
 
 
 class PessoaResponse(PessoaRequest):
     id: UUID
-    nascimento: date
