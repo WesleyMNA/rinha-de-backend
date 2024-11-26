f386bffb52778e1974f64f66505cd73a224e5c1e
Wesley Almeida <wesleymna98@gmail.com>
2024-11-25 21:11:27 -0300

    update

--- java/src/main/java/com/rinha/java/pessoas/PessoaRepository.java
+++ java/src/main/java/com/rinha/java/pessoas/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.rinha.java.pessoas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -38,4 +41,25 @@ public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
                 id = :id
             """, nativeQuery = true)
     Optional<PessoaResponse> findIdApelidoNomeNascimentoStackById(UUID id);
+
+    @Modifying
+    @Transactional
+    @Query("""
+            INSERT INTO
+            	Pessoa(
+            		id,
+            		apelido,
+            		nome,
+            		nascimento,
+            		stack
+            	)
+            VALUES
+            	(
+            		:id,
+            		:apelido,
+            		:nome,
+            		:nascimento,
+            		:stack
+            	)""")
+    void insert(UUID id, String apelido, String nome, LocalDate nascimento, String[] stack);
 }
