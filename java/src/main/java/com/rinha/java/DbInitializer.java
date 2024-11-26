6643bfd811c8818aa265fe3c7a7dba34114b99b7
Wesley Almeida <wesleymna98@gmail.com>
2024-11-25 21:00:21 -0300

    DbInitializer usa o HikariDataSource

--- java/src/main/java/com/rinha/java/DbInitializer.java
+++ java/src/main/java/com/rinha/java/DbInitializer.java
@@ -1,26 +1,19 @@
 package com.rinha.java;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.Statement;
 
 @Configuration
 public class DbInitializer {
 
-    private final String url;
-    private final String username;
-    private final String password;
+    private final HikariDataSource hikariDataSource;
 
-    public DbInitializer(@Value("${spring.datasource.url}") String url,
-                         @Value("${spring.datasource.username}") String username,
-                         @Value("${spring.datasource.password}") String password) {
-        this.url = url;
-        this.username = username;
-        this.password = password;
+    public DbInitializer(HikariDataSource hikariDataSource) {
+        this.hikariDataSource = hikariDataSource;
     }
 
     @PostConstruct
@@ -49,7 +42,7 @@ public class DbInitializer {
 
                 CREATE UNIQUE INDEX IF NOT EXISTS idx_pessoa_apelido ON pessoa USING btree (apelido);
                 """;
-        try (Connection conn = DriverManager.getConnection(url, username, password);
+        try (Connection conn = hikariDataSource.getConnection();
              Statement stmt = conn.createStatement()) {
             stmt.executeUpdate(query);
         } catch (Exception e) {
