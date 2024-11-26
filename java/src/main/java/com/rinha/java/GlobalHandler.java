501b3da3945bd7c51216274010eee871c7e3ed7a
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-11-26 08:28:09 -0300

    insert com string query

--- java/src/main/java/com/rinha/java/GlobalHandler.java
+++ java/src/main/java/com/rinha/java/GlobalHandler.java
@@ -1,6 +1,5 @@
 package com.rinha.java;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,14 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.format.DateTimeParseException;
+import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseEntity<Void>> handleGlobalException(Exception ex, WebRequest request) {
-        if (ex instanceof DataIntegrityViolationException || ex instanceof DateTimeParseException)
+        if (ex instanceof SQLException || ex instanceof IllegalArgumentException)
             return ResponseEntity
                     .status(HttpStatus.UNPROCESSABLE_ENTITY)
                     .build();
--- java/src/main/java/com/rinha/java/pessoas/PessoaController.java
+++ java/src/main/java/com/rinha/java/pessoas/PessoaController.java
@@ -1,11 +1,13 @@
 package com.rinha.java.pessoas;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -16,9 +18,12 @@ import java.util.UUID;
 public class PessoaController {
 
     private final PessoaRepository repository;
+    private final HikariDataSource hikariDataSource;
 
-    public PessoaController(PessoaRepository repository) {
+    public PessoaController(PessoaRepository repository,
+                            HikariDataSource hikariDataSource) {
         this.repository = repository;
+        this.hikariDataSource = hikariDataSource;
     }
 
     @GetMapping("/pessoas")
@@ -41,12 +46,33 @@ public class PessoaController {
 
     @PostMapping("/pessoas")
     public ResponseEntity<Void> create(@RequestBody @Valid PessoaRequest request)
-            throws URISyntaxException {
+            throws URISyntaxException, SQLException {
         validate(request);
-        var pessoa = new Pessoa(request);
-        repository.save(pessoa);
+        UUID id = UUID.randomUUID();
+        Date date = Date.valueOf(request.nascimento());
+        var query = """
+                INSERT INTO
+                	public.pessoa(
+                		id,
+                		apelido,
+                		nome,
+                		nascimento,
+                		stack
+                	)
+                VALUES
+                	(?, ?, ?, ?, ?);
+                """;
+        try (Connection conn = hikariDataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setObject(1, id);
+            stmt.setString(2, request.apelido());
+            stmt.setString(3, request.nome());
+            stmt.setDate(4, date);
+            stmt.setObject(5, request.stack());
+            stmt.executeUpdate();
+        }
         return ResponseEntity
-                .created(new URI("/pessoas/%s".formatted(pessoa.getId())))
+                .created(new URI("/pessoas/%s".formatted(id)))
                 .build();
     }
 
